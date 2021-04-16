b5306a7eaff964e93cbe8e240264ae7c6c2d9f6c
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2021-04-16 16:39:44 -0600

    initial commit

---
+++ src/main/java/mx/tec/kata/PalindromeKataApplication.java
@@ -0,0 +1,13 @@
+package mx.tec.kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalindromeKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalindromeKataApplication.class, args);
+	}
+
+}
